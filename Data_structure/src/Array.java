a7eba017227f081a0e98d4330c74b96560db08c7
MurlocKing <MurlocKing@DESKTOP-IFFMI3P>
2021-01-25 20:33:26 +0900

    주석 추가

--- Data_structure/src/Array.java
+++ Data_structure/src/Array.java
@@ -3,17 +3,32 @@ public class Array {
 	private int size;
 	private int cursor;
 	
+	/******************************
+	 * 이름: Array생성자
+	 * 기능: 비어있는 어레이 클래스를 만든다.
+	 * 작성자: 박찬솔
+	 *****************************/
 	public Array() {
-		this.arr = new int[100];
+		this.arr = new int[0];
 		this.cursor = -1;
-		this.size = 100;
+		this.size = 0;
 	}
 	public Array(int size) {
 		this.arr = new int[size];
 		this.cursor = -1;
 		this.size = size;
 	}
+	public Array(int[] arr) {
+		this.arr = arr;
+		this.cursor = -1;
+		this.size = arr.length;
+	}
 	
+	/******************************
+	 * 이름: Array 기본 연산
+	 * 기능: Array 크기 반환, 값을 채운 공간 크기 반환, 비었는지 가득찼는지 확인
+	 * 작성자: 박찬솔
+	 *****************************/
 	public int getSize() { return size; }
 	public int getCursor() { return cursor; }
 	public boolean isEmpty() {
@@ -25,12 +40,22 @@ public class Array {
 		else { return false; }
 	}
 	
+	/******************************
+	 * 이름: get메서드
+	 * 기능: 인덱스에 해당하는 값 반환
+	 * 작성자: 박찬솔
+	 *****************************/
 	public int get( int i ) {
 		if( i > cursor ) { return -2147483648; }
 		else { return arr[i]; }
 		
 	}
 	
+	/******************************
+	 * 이름: add메서드
+	 * 기능: 원하는 위치에 원하는 값 대입
+	 * 작성자: 박찬솔
+	 *****************************/
 	public boolean add(int value) {
 		if( isFull() ) { return false; }
 		else {
@@ -47,6 +72,11 @@ public class Array {
 		}
 	}
 	
+	/******************************
+	 * 이름: find메서드
+	 * 기능: 찾고자 하는 값에 대한 인덱스 반환
+	 * 작성자: 박찬솔
+	 *****************************/
 	public int find(int value) {
 		for(int i = 0; i <= cursor; i++) {
 			if( arr[i] == value ) { return i; }
@@ -54,6 +84,11 @@ public class Array {
 		return -1;
 	}
 	
+	/******************************
+	 * 이름: remove 메서드
+	 * 기능: 특정 값(1개)을 삭제
+	 * 작성자: 박찬솔
+	 *****************************/
 	public boolean remove(int value) {
 		int key = find(value);
 		if( key > 0 ) {
@@ -64,6 +99,11 @@ public class Array {
 		} else { return false; }
 	}
 	
+	/******************************
+	 * 이름: pop 메서드
+	 * 기능: 특정 인덱스를 반환 및 삭제 
+	 * 작성자: 박찬솔
+	 *****************************/
 	public int pop() {
 		if( isEmpty() ) { return -2147483648; }
 		else { return get(cursor--); }
@@ -76,6 +116,11 @@ public class Array {
 		return value;
 	}
 	
+	/******************************
+	 * 이름: toArray 메서드
+	 * 기능: 배열 자료형 반환
+	 * 작성자: 박찬솔
+	 *****************************/
 	public int[] toArray() {
 		return arr;
 	}
