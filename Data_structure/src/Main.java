08f538861bdef30e7e42f4fca65f40e8c0dae729
MurlocKing <MurlocKing@DESKTOP-IFFMI3P>
2021-01-27 19:05:49 +0900

    Vector구현

--- Data_structure/src/Main.java
+++ Data_structure/src/Main.java
@@ -1,31 +1,14 @@
 public class Main {
 	public static void main(String[] args) {
+		Vector_custom<Integer> a = new Vector_custom<Integer>();
+		a.add(3);
+		a.add(4);
+		Integer c[] = {1,2,3,4,5,9};
+		a.add(c);
+		System.out.println(a.pop());
 		
-		Array_custom<Integer> a = new Array_custom<Integer>(10);		
-		for(int i = 0; i < 10; i++) {
-			a.add(i);
-		}
-		System.out.println("Array_custom a");
-		for(int i = 0; i < 12; i++) {
-			System.out.println(i + ":" + a.get(i));
-		}
-		
-		Integer[] b= new Integer[10];
-        a.toArray(b);
-        System.out.println("array b");
-		for(int i = 0; i < 10; i++) {
-			System.out.println(i + ":" + b[i]);
-		}
-		
-		a.pop();
-		System.out.println("Array_custom after pop a");
-		for(int i = 0; i < 10; i++) {
-			System.out.println(i + ":" + a.get(i));
-		}
-		
-		System.out.println("array b");
-		for(int i = 0; i < 10; i++) {
-			System.out.println(i + ":" + b[i]);
-		}
+		Array_custom<Integer> b = new Array_custom<Integer>(1);
+		b.add(3);
+		System.out.println(b.pop());
 	}
 }
--- Data_structure/src/Vector_custom.java
+++ Data_structure/src/Vector_custom.java
@@ -1,37 +1,37 @@
 @SuppressWarnings("unchecked")
-public class Array_custom<T> {
-	T arr[];
+public class Vector_custom<T> {
+	T vec[];
 	private int capacity;
 	private int size;
 	
 	/******************************
-	 * 이름: Array생성자
+	 * 이름: Vector생성자
 	 * 기능: 비어있는 어레이 클래스를 만든다.
 	 * 작성자: 박찬솔
 	 *****************************/
-	public Array_custom() {
-		this.arr = (T[]) new Object[0];
+	public Vector_custom() {
+		this.vec = (T[]) new Object[0];
 		this.size = 0;
 		this.capacity = 0;
 	}
-	public Array_custom(int capacity) {
-		this.arr = (T[]) new Object[capacity];
+	public Vector_custom(int capacity) {
+		this.vec = (T[]) new Object[capacity];
 		this.size = 0;
 		this.capacity = capacity;
 	}
-	public Array_custom(T[] arr) {
-		this.arr = arr;
+	public Vector_custom(T[] arr) {
+		this.vec = arr;
 		this.size = 0;
-		this.capacity = arr.length;
+		this.capacity = vec.length;
 	}
 	
 	/******************************
-	 * 이름: Array 기본 연산
-	 * 기능: Array 크기 반환, 값을 채운 공간 크기 반환, 비었는지 가득찼는지 확인
+	 * 이름: Vector 기본 연산
+	 * 기능: Vector 크기 반환, 값을 채운 공간 크기 반환, 비었는지 가득찼는지 확인
 	 * 작성자: 박찬솔
 	 *****************************/
-	public int getSize() { return capacity; }
-	public int getCursor() { return size; }
+	public int getCapacity() { return capacity; }
+	public int getSize() { return size; }
 	public boolean isEmpty() {
 		if( size == 0 ) { return true; }
 		else { return false; }
@@ -42,14 +42,33 @@ public class Array_custom<T> {
 	}
 	
 	/******************************
+	 * 이름: grow메서드
+	 * 기능: 벡터의 크기를 키우는 함수
+	 * 작성자: 박찬솔
+	 *****************************/
+	private void grow() {
+	    T[] temp=(T[]) new Object[capacity];
+	    capacity++;
+	    toArray(temp);
+	    vec = (T[]) new Object[capacity];
+	    for(int i = 0; i < size; i++) { vec[i] = (T)temp[i]; }
+	}
+	private void grow(int x) {
+	    T[] temp=(T[]) new Object[capacity];
+	    capacity += x;
+	    toArray(temp);
+	    vec = (T[]) new Object[capacity];
+	    for(int i = 0; i < size; i++) { vec[i] = (T)temp[i]; }
+	}
+	
+	/******************************
 	 * 이름: get메서드
 	 * 기능: 인덱스에 해당하는 값 반환
 	 * 작성자: 박찬솔
 	 *****************************/
 	public T get( int i ) {
 		if( i > size ) { return null; }
-		else { return arr[i]; }
-		
+		else { return vec[i]; }
 	}
 	
 	/******************************
@@ -57,20 +76,22 @@ public class Array_custom<T> {
 	 * 기능: 원하는 위치에 원하는 값 대입
 	 * 작성자: 박찬솔
 	 *****************************/
-	public boolean add(T value) {
-		if( isFull() ) { return false; }
-		else {
-			arr[size++] = value;
-			return true;
-		}
+	public void add(T value) {
+		if( isFull() )
+			grow();
+		vec[size++] = value;
 	}
-	public boolean add(int key, T value) {
-		if(key >= capacity) { return false; }
-		else {
-			arr[key] = value;
-			if(key+1 > size) { size = key+1; }
-			return true;
-		}
+	public void add(int key, T value) {
+		if(key >= capacity)
+			grow(key-capacity+1);
+		vec[key] = value;
+		if(key+1 > size) { size = key+1; }
+	}
+	public void add(T[] arr) {
+		if(size+arr.length > capacity)
+			grow(size+arr.length-capacity);
+		for(int i = size; i < capacity; i++) { vec[i] = arr[i-size]; }
+		size += arr.length;
 	}
 	
 	/******************************
@@ -80,7 +101,7 @@ public class Array_custom<T> {
 	 *****************************/
 	public int find(T value) {
 		for(int i = 0; i < size; i++) {
-			if( arr[i] == value ) { return i; }
+			if( vec[i] == value ) { return i; }
 		}
 		return -1;
 	}
@@ -94,7 +115,7 @@ public class Array_custom<T> {
 		int key = find(value);
 		if( key > 0 ) {
 			for(int i = key; i < size; i++) {
-				arr[i] = arr[i+1];
+				vec[i] = vec[i+1];
 			}
 			return true;
 		} else { return false; }
@@ -107,12 +128,12 @@ public class Array_custom<T> {
 	 *****************************/
 	public T pop() {
 		if( isEmpty() ) { return null; }
-		else { return get(--size); }
+		return get(--size);
 	}
 	public T pop(int key) {
 		T value = get(key);
 		for(int i = key; i < size; i++) {
-			arr[i] = arr[i+1];
+			vec[i] = vec[i+1];
 		}
 		return value;
 	}
@@ -122,7 +143,7 @@ public class Array_custom<T> {
 	 * 기능: 배열 포인터를 매개변수로 받아 직접 데이터 대입
 	 * 작성자: 박찬솔
 	 *****************************/
-	public void toArray(T[] target) { for(int i = 0; i < size; i++) { target[i] = (T)arr[i]; } }
-	public void toArray(T[] target, int end){ for(int i = 0; i < end; i++) { target[i] = (T)arr[i]; } }
-	public void toArray(T[] target, int start,int end) { for(int i = start; i < end; i++) { target[i] = (T)arr[i]; } }
+	public void toArray(T[] target) { for(int i = 0; i < size; i++) { target[i] = (T)vec[i]; } }
+	public void toArray(T[] target, int end){ for(int i = 0; i < end; i++) { target[i] = (T)vec[i]; } }
+	public void toArray(T[] target, int start,int end) { for(int i = start; i < end; i++) { target[i] = (T)vec[i]; } }
 }
